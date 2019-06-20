ac2ff3db7f47fae186df02f1dab7572d24743b96
david <david@172.22.95.65>
2019-06-20 21:25:22 +0200

    listo

---
+++ src/parqueinfantil/ParqueInfantil.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parqueinfantil;
+
+/**
+ *
+ * @author david
+ */
+public class ParqueInfantil {
+   
+
+    public boolean estaCerrado() {
+        return false;        
+    }
+
+    void entraColumpios(Persona aThis) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void salirColumpios(Persona aThis) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void entraTobogan(Persona aThis) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void salirTobogan(Persona aThis) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void entraTiovivo(Persona aThis) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void salirTiovivo(Persona aThis) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+  
+    
+}
